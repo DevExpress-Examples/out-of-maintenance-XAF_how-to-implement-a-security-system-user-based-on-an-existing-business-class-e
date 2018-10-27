ab30aa9da219bf6f5e56fb7faaf1bc9935fc6d63
DevExpressExampleBot <fakeEmail>
2018-10-27 22:11:59 +0300

    Source auto update [skip ci]

---
+++ VB/ImplementCustomUserBasedOnExistingClassEF.Module/DatabaseUpdate/Updater.vb
@@ -0,0 +1,42 @@
+﻿Imports System
+Imports System.Linq
+Imports DevExpress.ExpressApp
+Imports DevExpress.Data.Filtering
+Imports DevExpress.Persistent.Base
+Imports DevExpress.ExpressApp.Updating
+Imports DevExpress.ExpressApp.Security
+Imports DevExpress.ExpressApp.EF
+Imports DevExpress.Persistent.BaseImpl.EF
+Imports DevExpress.Persistent.BaseImpl.EF.PermissionPolicy
+Imports ImplementCustomUserBasedOnExistingClassEF.Module.BusinessObjects
+
+Namespace ImplementCustomUserBasedOnExistingClassEF.Module.DatabaseUpdate
+    ' For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppUpdatingModuleUpdatertopic.aspx
+    Public Class Updater
+        Inherits ModuleUpdater
+
+        Public Sub New(ByVal objectSpace As IObjectSpace, ByVal currentDBVersion As Version)
+            MyBase.New(objectSpace, currentDBVersion)
+        End Sub
+        Public Overrides Sub UpdateDatabaseAfterUpdateSchema()
+            MyBase.UpdateDatabaseAfterUpdateSchema()
+            Dim adminEmployeeRole As EmployeeRole = ObjectSpace.FindObject(Of EmployeeRole)(New BinaryOperator("Name", "AdminRole"))
+            If adminEmployeeRole Is Nothing Then
+                adminEmployeeRole = ObjectSpace.CreateObject(Of EmployeeRole)()
+                adminEmployeeRole.Name = "AdminRole"
+                adminEmployeeRole.IsAdministrative = True
+            End If
+            Dim adminEmployee As Employee = ObjectSpace.FindObject(Of Employee)(New BinaryOperator("UserName", "Admin"))
+            If adminEmployee Is Nothing Then
+                adminEmployee = ObjectSpace.CreateObject(Of Employee)()
+                adminEmployee.UserName = "Admin"
+                adminEmployee.SetPassword("")
+                adminEmployee.EmployeeRoles.Add(adminEmployeeRole)
+            End If
+            ObjectSpace.CommitChanges()
+        End Sub
+        Public Overrides Sub UpdateDatabaseBeforeUpdateSchema()
+            MyBase.UpdateDatabaseBeforeUpdateSchema()
+        End Sub
+    End Class
+End Namespace
---
+++ VB/ImplementCustomUserBasedOnExistingClassEF.Module/Module.vb
@@ -0,0 +1,46 @@
+﻿Imports System
+Imports System.Text
+Imports System.Linq
+Imports DevExpress.ExpressApp
+Imports System.ComponentModel
+Imports DevExpress.ExpressApp.DC
+Imports System.Collections.Generic
+Imports DevExpress.Persistent.Base
+Imports DevExpress.ExpressApp.Model
+Imports DevExpress.ExpressApp.Actions
+Imports DevExpress.ExpressApp.Editors
+Imports DevExpress.ExpressApp.Updating
+Imports DevExpress.ExpressApp.Model.Core
+Imports DevExpress.ExpressApp.Model.DomainLogics
+Imports DevExpress.ExpressApp.Model.NodeGenerators
+Imports System.Data.Entity
+Imports ImplementCustomUserBasedOnExistingClassEF.Module.BusinessObjects
+
+Namespace ImplementCustomUserBasedOnExistingClassEF.Module
+    ' For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppModuleBasetopic.aspx.
+    Public NotInheritable Partial Class ImplementCustomUserBasedOnExistingClassEFModule
+        Inherits ModuleBase
+
+        Shared Sub New()
+            DevExpress.Data.Linq.CriteriaToEFExpressionConverter.SqlFunctionsType = GetType(System.Data.Entity.SqlServer.SqlFunctions)
+            DevExpress.Data.Linq.CriteriaToEFExpressionConverter.EntityFunctionsType = GetType(System.Data.Entity.DbFunctions)
+            ' Uncomment this code to delete and recreate the database each time the data model has changed.
+            ' Do not use this code in a production environment to avoid data loss.
+            ' #if DEBUG
+            ' Database.SetInitializer(new DropCreateDatabaseIfModelChanges<ImplementCustomUserBasedOnExistingClassEFDbContext>());
+            ' #endif 
+        End Sub
+        Public Sub New()
+            InitializeComponent()
+            DevExpress.ExpressApp.Security.SecurityModule.UsedExportedTypes = DevExpress.Persistent.Base.UsedExportedTypes.Custom
+        End Sub
+        Public Overrides Function GetModuleUpdaters(ByVal objectSpace As IObjectSpace, ByVal versionFromDB As Version) As IEnumerable(Of ModuleUpdater)
+            Dim updater As ModuleUpdater = New DatabaseUpdate.Updater(objectSpace, versionFromDB)
+            Return New ModuleUpdater() { updater }
+        End Function
+        Public Overrides Sub Setup(ByVal application As XafApplication)
+            MyBase.Setup(application)
+            ' Manage various aspects of the application UI and behavior at the module level.
+        End Sub
+    End Class
+End Namespace
